b8daf723407c4c948732a709a0de441db0fe2215
whoami <rich.john.isom@gmail.com>
2023-04-18 15:27:16 -0700

    Update arractors.py
    *crosses fingers*

--- arractors.py
+++ arractors.py
@@ -37,9 +37,9 @@ def dadras(xyz, *, a=3, b=2.7, c=1.7, d=2, e=9):
 
 def halvorsen(xyz, *, a=1.89):
     x, y, z = xyz
-    x_dot = (-abs(a))*x-(4*y)-(4*z)-(y**2)
-    y_dot = (−abs(a))*y-(4*z)-(4*x)-(z**2)
-    z_dot = (−abs(a))*z-(4*x)-(4*y)+(x**2)
+    x_dot = ((a)*-1)*x+(y*-4)+(z*-4)-(y**2)
+    y_dot = ((a)*-1)*y+(z*-4)+(x*-4)-(z**2)
+    z_dot = ((a)*-1))*z+(x*-4)+(y*-4)+(x**2)
     
     return np.array([x_dot, y_dot, z_dot])
 
