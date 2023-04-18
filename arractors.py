d954c90858e3f4e260741559f428df041552b4ed
whoami <rich.john.isom@gmail.com>
2023-04-18 14:54:59 -0700

    Update arractors.py
    put into plot function

--- arractors.py
+++ arractors.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+dt = 0.01
+num_steps = 1000
+
 def lorenz(xyz, *, s=10, r=28, b=2.667):
     """
     Parameters
@@ -21,27 +24,28 @@ def lorenz(xyz, *, s=10, r=28, b=2.667):
     x_dot = s*(y - x)
     y_dot = r*x - y - x*z
     z_dot = x*y - b*z
+    
     return np.array([x_dot, y_dot, z_dot])
 
+def createPlot(title, attractorType):
+    xyzs = np.empty((num_steps + 1, 3))  # Need one more for the initial values
+    xyzs[0] = (0., 1., 1.05)  # Set initial values
+    # Step through "time", calculating the partial derivatives at the current point
+    # and using them to estimate the next point
+    for i in range(num_steps):
+        xyzs[i + 1] = xyzs[i] + attractorType(xyzs[i]) * dt
 
-dt = 0.01
-num_steps = 10000
+    # Plot
+    ax = plt.figure().add_subplot(projection='3d')
 
-xyzs = np.empty((num_steps + 1, 3))  # Need one more for the initial values
-xyzs[0] = (0., 1., 1.05)  # Set initial values
-# Step through "time", calculating the partial derivatives at the current point
-# and using them to estimate the next point
-for i in range(num_steps):
-    xyzs[i + 1] = xyzs[i] + lorenz(xyzs[i]) * dt
+    ax.plot(*xyzs.T, lw=0.5)
+    ax.set_xlabel("X Axis")
+    ax.set_ylabel("Y Axis")
+    ax.set_zlabel("Z Axis")
+    ax.set_title(title)
+     
+    return ax.figure
 
-# Plot
-ax = plt.figure().add_subplot(projection='3d')
+plot1 = createPlot('Lorenz Attractor',lorenz)
 
-ax.plot(*xyzs.T, lw=0.5)
-ax.set_xlabel("X Axis")
-ax.set_ylabel("Y Axis")
-ax.set_zlabel("Z Axis")
-ax.set_title("Lorenz Attractor")
-
-
-st.pyplot(ax.figure)
+st.pyplot(plot1.figure)
